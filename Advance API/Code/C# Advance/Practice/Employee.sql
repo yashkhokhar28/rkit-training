af8a2be233bfc1c88573dd1264e7ccc7c590e027
yashkhokhar28 <yashkhokhar28@gmail.com>
2025-01-09 17:48:58 +0530

    code update

--- Advance API/Code/C# Advance/Practice/Employee.sql
+++ Advance API/Code/C# Advance/Practice/Employee.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE EmployeeDB;
 USE EmployeeDB;
 
-CREATE TABLE Employees (
+CREATE TABLE EMP01 (
     P01F01 INT PRIMARY KEY AUTO_INCREMENT,
     P01F02 VARCHAR(255) NOT NULL,
     P01F03 INT NOT NULL,
@@ -9,4 +9,20 @@ CREATE TABLE Employees (
     P01F05 DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     P01F06 DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+INSERT INTO EMP01 (P01F02, P01F03, P01F04)
+VALUES 
+('Tony Stark', 45, 'Adult'),
+('Peter Parker', 16, 'Minor'),
+('Steve Rogers', 101, 'Adult'),
+('Natasha Romanoff', 35, 'Adult'),
+('Bruce Banner', 40, 'Adult'),
+('Wanda Maximoff', 25, 'Adult'),
+('Loki Laufeyson', 17, 'Minor'),
+('Thor Odinson', 1500, 'Adult'),
+('Clint Barton', 35, 'Adult'),
+('Shuri', 18, 'Adult');
+
+SELECT * FROM EMP01
+
 
