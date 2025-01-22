1743e20e1dca6dae1e5e8843efefd5d56c732a94
yashkhokhar28 <yashkhokhar28@gmail.com>
2025-01-22 18:51:10 +0530

    dot-net core started

---
+++ DotNet Core/Code/.Net Core Fundamental/Practice/FundamentalDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FundamentalDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
