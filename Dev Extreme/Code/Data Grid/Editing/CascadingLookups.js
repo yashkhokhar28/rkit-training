7731366063e637f847206f0274ccda0bee00220e
Yash Khokhar <yashkhokhar28@gmail.com>
2025-02-17 12:37:37 +0530

    code update

--- Dev Extreme/Code/Data Grid/Editing/CascadingLookups.js
+++ Dev Extreme/Code/Data Grid/Editing/CascadingLookups.js
@@ -1,6 +1,6 @@
 $(() => {
   $("#gridContainer").dxDataGrid({
-    dataSource: cities, // Should be cities if managing city-level data
+    dataSource: cities,
     showBorders: true,
     editing: {
       allowUpdating: true,
@@ -8,19 +8,36 @@ $(() => {
       mode: "row",
     },
     onEditorPreparing(e) {
+      // Disable the City field if State is not selected
       if (e.parentType === "dataRow" && e.dataField === "CityID") {
-        const isStateNotSet = !e.row.data.StateID;
+        const isStateNotSet = !e.row.data.StateID === undefined;
         e.editorOptions.disabled = isStateNotSet;
       }
+
+      // Disable the State field if Country is not selected
       if (e.parentType === "dataRow" && e.dataField === "StateID") {
-        const isCountryNotSet = !e.row.data.CountryID;
+        const isCountryNotSet = !e.row.data.CountryID === undefined;
         e.editorOptions.disabled = isCountryNotSet;
+
+        // Filter states based on the selected Country
+        if (e.row.data.CountryID) {
+          e.editorOptions.dataSource = states.filter(
+            (state) => state.CountryID === e.row.data.CountryID
+          );
+        } else {
+          e.editorOptions.dataSource = states; // Show all states if no country is selected
+        }
       }
     },
     columns: [
       {
         dataField: "CountryID",
         caption: "Country",
+        setCellValue(rowData, value) {
+          rowData.CountryID = value;
+          rowData.StateID = null;
+          rowData.CityID = null;
+        },
         lookup: {
           dataSource: countries,
           valueExpr: "CountryID",
@@ -35,7 +52,7 @@ $(() => {
           rowData.CityID = null; // Reset city when state changes
         },
         lookup: {
-          dataSource: states,
+          dataSource: states, // Initially all states will be shown
           valueExpr: "StateID",
           displayExpr: "StateName",
         },
