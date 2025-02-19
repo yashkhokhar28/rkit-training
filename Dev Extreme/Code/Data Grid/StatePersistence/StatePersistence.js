80abdf8b173fb9067095760ff00141ec1cd6d257
Yash Khokhar <yashkhokhar28@gmail.com>
2025-02-19 17:09:20 +0530

    code update

---
+++ Dev Extreme/Code/Data Grid/StatePersistence/StatePersistence.js
@@ -0,0 +1,84 @@
+$(() => {
+  const dataGrid = $("#gridContainer")
+    .dxDataGrid({
+      dataSource: marvelCharacters,
+      showBorders: true,
+      keyExpr: "ID",
+      stateStoring: {
+        enabled: false,
+        type: "localStore",
+      },
+      columns: [
+        {
+          caption: "Names",
+          columnMinWidth: 70,
+          alignment: "center",
+          columns: [
+            {
+              dataField: "Name",
+              caption: "Character Name",
+              alignment: "center",
+            },
+            {
+              dataField: "Real_Name",
+              caption: "Real Name",
+              alignment: "center",
+            },
+          ],
+        },
+        {
+          dataField: "Affiliation",
+          caption: "Team/Group",
+          dataType: "string",
+          alignment: "center",
+          width: 100,
+        },
+        {
+          dataField: "First_Appearance",
+          caption: "First Appearance",
+          dataType: "number",
+          alignment: "center",
+          columnMinWidth: 70,
+        },
+        {
+          dataField: "Abilities",
+          caption: "Superpowers",
+          dataType: "string",
+          alignment: "center",
+          columnMinWidth: 70,
+        },
+        {
+          dataField: "Status",
+          caption: "Status",
+          dataType: "string",
+          columnMinWidth: 70,
+          alignment: "center",
+          //   visible: false,
+        },
+      ],
+    })
+    .dxDataGrid("instance");
+
+  $("#save").dxButton({
+    text: "Save State",
+    onClick: function () {
+      const state = dataGrid.state();
+      localStorage.setItem("dataGridState", JSON.stringify(state));
+    },
+  });
+
+  $("#hide").dxButton({
+    text: "Hide Column",
+    onClick: function () {
+      dataGrid.columnOption(6, "visible", false);
+    },
+  });
+
+  $("#load").dxButton({
+    text: "Load State",
+    onClick: function () {
+      const state = JSON.parse(localStorage.getItem("dataGridState"));
+      dataGrid.state(state);
+    },
+  });
+});
