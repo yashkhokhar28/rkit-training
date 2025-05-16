11ae2b288088b8f616e970a89ea22234339a47d1
yashkhokhar28 <yashkhokhar28@gmail.com>
2025-05-16 13:23:17 +0530

    script changes

--- research-and-development/scripts/useful-command.sh
+++ research-and-development/scripts/useful-command.sh
@@ -15,21 +15,21 @@ CREATE TABLE IF NOT EXISTS orders10 (id INT, item VARCHAR(50));
 
 for i in {1..10}; do
   mysql -u root -e "
-    INSERT INTO test.orders1 VALUES ($i, 'Item1_$i');
-    INSERT INTO test.orders2 VALUES ($i, 'Item2_$i');
-    INSERT INTO test.orders3 VALUES ($i, 'Item3_$i');
-    INSERT INTO test.orders4 VALUES ($i, 'Item4_$i');
-    INSERT INTO test.orders5 VALUES ($i, 'Item5_$i');
-    INSERT INTO test.orders6 VALUES ($i, 'Item6_$i');
-    INSERT INTO test.orders7 VALUES ($i, 'Item7_$i');
-    INSERT INTO test.orders8 VALUES ($i, 'Item8_$i');
-    INSERT INTO test.orders9 VALUES ($i, 'Item9_$i');
-    INSERT INTO test.orders10 VALUES ($i, 'Item10_$i');
+    INSERT INTO test_1.orders1 VALUES ($i, 'Item1_$i');
+    INSERT INTO test_1.orders2 VALUES ($i, 'Item2_$i');
+    INSERT INTO test_1.orders3 VALUES ($i, 'Item3_$i');
+    INSERT INTO test_1.orders4 VALUES ($i, 'Item4_$i');
+    INSERT INTO test_1.orders5 VALUES ($i, 'Item5_$i');
+    INSERT INTO test_1.orders6 VALUES ($i, 'Item6_$i');
+    INSERT INTO test_1.orders7 VALUES ($i, 'Item7_$i');
+    INSERT INTO test_1.orders8 VALUES ($i, 'Item8_$i');
+    INSERT INTO test_1.orders9 VALUES ($i, 'Item9_$i');
+    INSERT INTO test_1.orders10 VALUES ($i, 'Item10_$i');
   "
 done
 
 mysql -u root -e "
-USE test;
+USE test_1;
 SELECT 'orders1' AS table_name, COUNT(*) FROM orders1
 UNION
 SELECT 'orders2', COUNT(*) FROM orders2
@@ -49,4 +49,18 @@ UNION
 SELECT 'orders9', COUNT(*) FROM orders9
 UNION
 SELECT 'orders10', COUNT(*) FROM orders10;
+"
+
+mysql -u root -e "
+DROP DATABASE IF EXISTS test;
+DROP DATABASE IF EXISTS test_1;
+DROP DATABASE IF EXISTS test_2;
+DROP DATABASE IF EXISTS test_3;
+DROP DATABASE IF EXISTS test_4;
+DROP DATABASE IF EXISTS test_5;
+DROP DATABASE IF EXISTS test_6;
+DROP DATABASE IF EXISTS test_7;
+DROP DATABASE IF EXISTS test_8;
+DROP DATABASE IF EXISTS test_9;
+DROP DATABASE IF EXISTS test_10;
 "
